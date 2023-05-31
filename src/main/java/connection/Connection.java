2437815aaef3784066425f2f6be98b02c503483a
saber <sbaerm.com@gmail.com>
2023-05-31 11:29:49 +0330

    create database and packages and classes

--- src/main/java/connection/Connection.java
+++ src/main/java/connection/Connection.java
@@ -1,4 +1,16 @@
 package connection;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Connection {
+    public static Connection getConnections() {
+        try {
+            return (Connection) DriverManager.getConnection("jdbc:postgresql://localhost:5432/writer", "postgres", "56275627");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
+
